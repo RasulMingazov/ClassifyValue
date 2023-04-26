0d95b265d683d4188fb44941f82c1435834f7b2c
RasulMingazov <mingazovri1@sovcombank.ru>
2023-04-26 03:31:45 +0300

    feat: Add custom theme

---
+++ theme/src/main/java/com/jeanbernad/classifyvalue/theme/colors/local/LocalColors.kt
@@ -0,0 +1,31 @@
+package com.jeanbernad.classifyvalue.theme.colors.local
+
+import androidx.compose.ui.graphics.Color
+
+interface LocalColors {
+
+    fun pink(): Color
+
+    fun darkPink(): Color
+
+    fun lightPink(): Color
+
+    fun partyPink(): Color
+
+    fun bottomPink(): Color
+
+    fun purple(): Color
+
+    fun darkPurple(): Color
+
+    fun lightLime(): Color
+
+    fun lime(): Color
+
+    fun black(): Color
+
+    fun white(): Color
+
+    fun grayPurple(): Color
+
+}
---
+++ theme/src/main/java/com/jeanbernad/classifyvalue/theme/typography/TypographyTypes.kt
@@ -0,0 +1,15 @@
+package com.jeanbernad.classifyvalue.theme.typography
+
+import androidx.compose.ui.text.TextStyle
+
+interface TypographyTypes {
+
+    fun text12Bold(): TextStyle
+
+    fun text14(): TextStyle
+
+    fun text16(): TextStyle
+
+    fun text16Bold(): TextStyle
+
+}
